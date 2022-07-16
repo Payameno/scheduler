a49472d6fa9fb78f144e1af8a4240274863377dc
Payam Beigi <beigi.payam@gmail.com>
2022-07-15 17:36:37 -0700

    Add helper function to find appoints by day

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,72 +1,29 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import DayList from "./DayList.jsx";
 
 import "components/Application.scss";
 import Appointment from "components/Appointment"
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  }, []);
 
-  const appointmentsArray = Object.values(appointments).map((appointment) =>{
+  const setDay = day => setState({...state, day})
+  const setDays = (days) => {setState(prev => ({...prev, days}))}
+
+  const dailyAppointments = [];
+
+  useEffect(() => {
+    Axios.get('/api/days').then(response => setDays(response.data));
+    Axios.get('/api/appointments').then(response => );
+  },[]);
+
+  const appointmentsArray = dailyAppointments.map((appointment) =>{
     return (
       <Appointment
       key={appointment.id}
@@ -86,8 +43,8 @@ export default function Application(props) {
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
 <DayList
-  days={days}
-  value={day}
+  days={state.days}
+  value={state.day}
   onChange={setDay}
 />
 </nav>
---
+++ src/helpers/selectors.js
@@ -0,0 +1,16 @@
+export default function getAppointmentsForDay(state, day) {
+  let appointments = [];
+  const dayappointments = [];
+  for (let dayitem of state.days) {
+    if (dayitem.name === day) {
+      appointments = dayitem.appointments;
+    }
+  }
+
+  for (let appointmnetId of appointments) {
+    if (state.appointments[appointmnetId]) {
+      dayappointments.push(state.appointments[appointmnetId])
+    }
+  }
+  return dayappointments;
+}
