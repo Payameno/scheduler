750fd5e7605a126636e040351b8103564a630195
Payam Beigi <beigi.payam@gmail.com>
2022-07-18 18:19:04 -0700

    Add stats messsge, before saving is completed,  status appears

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -39,6 +39,8 @@ export default function Application(props) {
     }));
   }
 
+  
+
     return (
       <Appointment
       key={appointment.id}
--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -3,11 +3,13 @@ import Form from "./Form";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 export default function Apppointment(props) {
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
@@ -17,12 +19,14 @@ export default function Apppointment(props) {
       student: name,
       interviewer
     };
+    transition(SAVING);
     props.bookInterview(props.id, interview).then(() => transition(SHOW));
   }
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SAVING && <Status message={"Saving the appointment"}/>}
       {mode === SHOW && props.interview && (
         <Show
           student={props.interview.student}
